bea80ac0a743908ade3b0b0bb07ef30535bc4d80
xxf_cz <xxf_cz@163.com>
2018-11-19 23:45:49 +0800

    向数据库添加示例数据

--- tests/db_test.js
+++ tests/db_test.js
@@ -1,25 +1,56 @@
-const sequelize = require("../db")
+const sequelize = require('../db')
 
-const { Workshop } = require("../models")
+const {
+  Workshop,
+  CardType,
+  Procedure,
+  CheckItem,
+  CheckCard
+} = require('../models')
 
 let initDb = async (force = true) => {
   try {
     await sequelize.sync({ force })
 
     await Workshop.create({
-        id: 1,
-        name: '衡阳供电段'
+      id: 1,
+      name: '衡阳供电段'
     })
     await Workshop.create({
-        id: 2,
-        name: '衡阳东高铁供电车间'
+      id: 2,
+      name: '衡阳东高铁供电车间'
     })
 
+    await CardType.create({
+      id: 1,
+      name: '接触网作业检查卡'
+    })
+
+    let ciid = 1
+    for (let i = 1; i <= 6; i++) {
+      let o = await Procedure.create({
+        id: i,
+        cardtypeId: 1,
+        name: '步骤' + i
+      })
+      let pid = o.id
+
+      for (let j = 1; j <= 3; j++) {
+        await CheckItem.create({
+          procedureId: pid,
+          name: `检查项目 ${i}.${j}`
+        })
+      }
+    }
   } catch (err) {
-    console.log("===============================================")
-    console.error("initDb(): Error occurred:", err)
+    console.log('===============================================')
+    console.error('initDb(): Error occurred:', err)
   }
 }
 
-initDb()
-//process.exit(0)
+let run = async () => {
+  await initDb()
+  process.exit(0)
+}
+
+run()
