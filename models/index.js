478ecb2111cd916dc4c838aaa3788b801b265735
xxf_cz <xxf_cz@163.com>
2018-11-19 23:45:32 +0800

    数据库模型

--- models/index.js
+++ models/index.js
@@ -7,6 +7,39 @@ const Workshop = sequelize.define('workshop', {
   }
 })
 
+// 检查卡类型：作业卡，辅助作业卡，登乘卡，......
+const CardType = sequelize.define('cardtype', {
+  name: Sequelize.STRING
+})
+
+// 流程
+const Procedure = sequelize.define('procedure', {
+  name: Sequelize.STRING
+})
+
+Procedure.belongsTo(CardType)
+CardType.hasMany(Procedure)
+
+// 盯控项目
+const CheckItem = sequelize.define('checkitem', {
+  name: Sequelize.STRING
+})
+
+CheckItem.belongsTo(Procedure)
+Procedure.hasMany(CheckItem)
+
+// 检查卡
+const CheckCard = sequelize.define('checkcard', {
+  // 1=没有问题，2=无此项目，3=存在问题
+  choice: Sequelize.SMALLINT,
+  problem: Sequelize.STRING
+})
+
 module.exports = {
-  Workshop
+  Workshop,
+  CardType,
+  Procedure,
+  CheckItem,
+  CheckCard
 }
+
