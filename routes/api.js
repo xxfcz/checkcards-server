dbe417d45c0bbe96849086044d64caeae287e11c
xxf_cz <xxf_cz@163.com>
2018-11-22 17:16:20 +0800

    +) 检查卡页面需要的api

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
-const { CardType } = require('../models')
+const { CardType, CheckItem, Procedure } = require('../models')
+const logger = require('koa-logger')
 
 module.exports = (router) => {
   router.get('/api/cardtypes', async function (ctx, next) {
@@ -7,4 +8,43 @@ module.exports = (router) => {
     })
     ctx.body = data
   })
+
+  router.get('/api/cardtypes/:id', async (ctx,next)=>{
+    let id = ctx.params.id
+    let data = await CardType.findById(id)
+    ctx.body = data
+  })
+
+  router.get('/api/checkitems/type/:id', async function (ctx, next) {
+    let typeId = ctx.params.id
+    console.log(typeId)
+    let data = await CheckItem.findAll({
+      include: [
+        {
+          model: Procedure,
+          where: {
+            cardtypeId: typeId
+          }
+        }
+      ]
+    })
+    //console.log(data)
+    ctx.body = data
+  })
+
+  router.get('/api/procedures/type/:id', async function (ctx, next) {
+    let typeId = ctx.params.id
+    console.log(typeId)
+    let data = await Procedure.findAll({
+      where: {
+        cardtypeId: typeId
+      },
+      include: [{
+        model: CheckItem
+      }]
+    })
+    //console.log(data)
+    ctx.body = data
+  })
+
 }
